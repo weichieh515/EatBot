1abd8971952ac6e51d2bf52c8b01571deb9a360b
Jack Hsu <ace05152535@gmail.com>
2018-01-01 12:50:19 +0800

    change err message to json

--- app/helper/errHandle.js
+++ app/helper/errHandle.js
@@ -3,5 +3,7 @@ const callerId = require('caller-id');
 module.exports = (err, res) => {
     let caller = callerId.getData();
     console.log(`[ERROR] <${caller.functionName}>\nMessage:${err.message}\n`)
-    res.send(err.message).status(400);
+    res.json({
+        err: err.message
+    }).status(400);
 }
